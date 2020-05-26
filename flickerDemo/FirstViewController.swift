fb659b30b4986c7afc671201a04cb49e454cb212
william <talk8122@gmail.com>
2020-05-26 15:09:19 +0800

    Initial Commit

---
+++ flickerDemo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  flickerDemo
+//
+//  Created by william on 2020/5/26.
+//  Copyright © 2020 william. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ flickerDemo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  flickerDemo
+//
+//  Created by william on 2020/5/26.
+//  Copyright © 2020 william. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
